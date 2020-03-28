08f9ed7d6eeeb1335a52dc9ce293d1127c3c1838
Catherine Barnett <ca.barnett06@gmail.com>
2020-03-28 18:47:16 -0400

    adding components

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Header from "./components/Header";
 import './App.css';
+import employees from "./employees.json";
+import Searchbar from "./components/Searchbar";
+import Tbl from "./components/Tbl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  // setting initial state
+  state = {
+    employees,
+    search: ""
+  };
+
+ findEmployee = emp => {
+   console.log(emp);
+  //  -1 with .indexof means whatever you're looking for is not there--so by saying !== whatever you're returning whatever you're searching for if it IS in the directory
+   if ((emp.name).indexOf(this.state.search) !== -1) {
+    return emp;
+    // to do: toLowerCase both emp.name and this.search.state so that it won't snag if someone doesn't type it in just right thing
+   } 
+
+ }
+
+  // When the form is submitted, search the employees.json for `this.state.search`
+  handleFormSubmit = event => {
+
+    event.preventDefault();
+    const filterEmp = employees.filter(this.findEmployee);
+    console.log("search"+ this.state.search);
+    this.setState({ employees: filterEmp });
+    }
+  
+
+  handleInputChange = event => {
+    console.log(event.target.value);
+    this.setState({ search: event.target.value })
+  };
+
+ 
+
+
+  // function for sorting employees in alphabetical order
+
+
+  // we want to map over employees and render a row for each employee
+   render () {
+    return (
+      <div className="container">
+        <Header>
+          <h1>Employee Directory</h1>
+        </Header>
+
+        <Searchbar 
+        search={this.state.search}
+        handleFormSubmit={this.handleFormSubmit}
+        handleInputChange={this.handleInputChange}
+        />
+
+        <table>
+
+                <tr>
+                    <th>ID</th>
+                    <th>Pic</th>
+                    <th>Name <button type="button" className="btn btn-primary">sort</button></th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                </table>
+        {this.state.employees.map(employee => (
+        <Tbl
+          id ={employee.id}
+          key={employee.id}
+          name={employee.name}
+          image={employee.image}
+          phone={employee.phone}
+          email={employee.email}
+        />
+        ))}
+      </div>
+
+    );
+  }
 }
+
 
 export default App;
